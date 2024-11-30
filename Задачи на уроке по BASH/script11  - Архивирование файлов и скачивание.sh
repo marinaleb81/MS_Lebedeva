d25fb471b6caf638439966603ac4981335d3d9c6
marinaleb81 <fed.mafy@gmail.com>
2024-11-30 16:36:15 +0300

    Правки 11 скрипта, нашла неточность!

--- Задачи на уроке по BASH/script11  - Архивирование файлов и скачивание.sh
+++ Задачи на уроке по BASH/script11  - Архивирование файлов и скачивание.sh
@@ -10,22 +10,19 @@ REMOTE_ARCHIVE="/tmp/$ARCHIVE_NAME"
 # Архивация на удалённом сервере (Вариант со скриптом)
 ssh $REMOTE_SERVER "tar -czf /tmp/$ARCHIVE_NAME -C $REMOTE_DIR ."
 
-# через PowerShell
-ssh user@192.168.31.83 "tar -czf /tmp/backup_$(date +%F).tar.gz -C /home/user/MS_Lebedeva ."
-
 # Разархивация на удалённом сервере (Вариант со скриптом)
 ssh $REMOTE_SERVER "mkdir -p $REMOTE_EXTRACT_DIR && tar -xzf $REMOTE_ARCHIVE -C $REMOTE_EXTRACT_DIR"
 
 # через PowerShell
 
 # Создание архива на удалённом сервере
-ssh user@192.168.31.83 "tar -czf /tmp/backup_$(date +%F).tar.gz -C /path/to/source_dir ."
+ssh user@192.168.31.83 "tar -czf /home/user/MS_Lebedeva/backup_$(date +%F).tar.gz -C /home/user/MS_Lebedeva ."
 
 # Скачивание архива на локальную машину
-scp user@192.168.31.83:/tmp/backup_$(date +%F).tar.gz "e:/Вэб инжиниринг/Задачи на уроке SSH/backup/backup_$(date +%F).tar.gz"
+scp user@192.168.31.83:/home/user/MS_Lebedeva/backup_$(date +%F).tar.gz "e:/Вэб инжиниринг/Задачи на уроке SSH/backup/backup_$(date +%F).tar.gz"
 
 # Разархивация на локальной машине
 tar -xzf "e:/Вэб инжиниринг/Задачи на уроке SSH/backup/backup_$(date +%F).tar.gz" -C "e:/Вэб инжиниринг/Задачи на уроке SSH/backup/extracted/"
 
 # Удаление архива на удалённом сервере
-ssh user@192.168.31.83 "rm /tmp/backup_$(date +%F).tar.gz"
+ssh user@192.168.31.83 "rm /home/user/MS_Lebedeva/backup_$(date +%F).tar.gz"
